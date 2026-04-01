daab531fe596d182596aaf02aca92232797dce93
Petar Stoev <petarstoev0505@gmail.com>
2026-04-01 10:02:24 +0300

    feat(registry): add public API and verify build

---
+++ packages/registry/src/index.ts
@@ -0,0 +1,5 @@
+export type { Chain, Token } from "./types.js";
+
+export { getAllChains, getChain, getChainsByEcosystem } from "./chains.js";
+
+export { getAllTokens, getToken, getTokensByChain } from "./tokens.js";
